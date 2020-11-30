5f8417662ea39d8442aef765fc7d06ea2281cb97
Dan Kulinkovich <dankulinkovich@gmail.com>
2020-11-30 22:35:52 +0300

    it's so simple recursion

---
+++ recursion.cpp
@@ -0,0 +1,16 @@
+﻿//simple recursion
+#include <iostream>
+using namespace std;
+
+int factorial(int val)
+{
+	if (val > 1)  return val* factorial(val - 1);
+	if (val < 0) return 0;
+	if (val == 0) return 1;
+}
+
+int main()
+{
+	cout << factorial(5);
+	return 0;
+}
