26e47219faa7403d0b4455c3536818b4df5e89ce
Dan Kulinkovich <dankulinkovich@gmail.com>
2020-12-13 21:29:16 +0200

    Add files via upload

---
+++ euclidean_algorithm.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int find_max(int a, int b)
+{
+	int del;
+	while (a > 0 && b > 0)
+	{
+		if (a > b) a = a % b;
+		else b = b % a;
+	}
+	del = a + b;
+	return del;
+}
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	cout << find_max(a, b);
+	return 0;
+}
